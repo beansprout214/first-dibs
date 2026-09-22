ae2c399c8c06a42e081bded3dbced3001546ff69
beansprout214 <brightest21@outlook.com>
2026-09-22 00:13:22 -0400

    added garment type

---
+++ frontend/src/types/Garment.ts
@@ -0,0 +1,9 @@
+export type Garment = {
+  id: number;
+  name: string;
+  size: string | null;
+  description: string | null;
+  photo_url: string | null;
+  claimant_name: string | null;
+  claimed_at: string | null;
+};
